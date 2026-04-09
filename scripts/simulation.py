576bee72f8fd2f666ebba5e80af69de64483e8fc
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-04-09 14:12:30 +0200

    remove unused import

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import MultiTaskLassoCV
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-from collasso import simulate, SingleTaskLassoCV, CoopLasso, CoopLassoCV
+from collasso import simulate, SingleTaskLassoCV, CoopLassoCV
 
 rho = [0, 0.90]
 prob_com = [0.00, 0.05]
