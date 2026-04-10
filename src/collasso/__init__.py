9ffa67e83ec68b034c59ee30594e8e247d4e5d25
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-04-10 19:40:15 +0200

    tidy up website

--- src/collasso/__init__.py
+++ src/collasso/__init__.py
@@ -12,14 +12,3 @@ from .functions import (
 
 __all__ = ["simulate", "SingleTaskLassoCV", "CoopLasso", "CoopLassoCV"]
 
-__pdoc__ = {
-    "SingleTaskLassoCV.set_score_request": False,
-    "CoopLasso.set_score_request": False,
-    "CoopLassoCV.set_score_request": False,
-    "SingleTaskLassoCV.set_fit_request": False,
-    "CoopLasso.set_fit_request": False,
-    "CoopLassoCV.set_fit_request": False,
-    "SingleTaskLassoCV.set_predict_request": False,
-    "CoopLasso.set_predict_request": False,
-    "CoopLassoCV.set_predict_request": False,
-}
