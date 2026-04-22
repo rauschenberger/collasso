edff7a8d49eb26bc4851894a3f6b8ba24860c9d6
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-04-22 09:54:44 +0200

    format code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,21 +2,21 @@
 import sys
 import os
 
-project = 'collasso'
-copyright = '2026, Armin Rauschenberger'
-author = 'Armin Rauschenberger'
-release = '0.1.0'
+project = "collasso"
+copyright = "2026, Armin Rauschenberger"
+author = "Armin Rauschenberger"
+release = "0.1.0"
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
-extensions = ["sphinx.ext.autodoc",  "numpydoc", "myst_parser"]
-source_suffix = ['.rst', '.md']
+extensions = ["sphinx.ext.autodoc", "numpydoc", "myst_parser"]
+source_suffix = [".rst", ".md"]
 
 numpydoc_show_class_members = False
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 
 html_theme_options = {
     "github_url": "https://github.com/rauschenberger/collasso",
