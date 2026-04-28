7fc8725a551e38b78dcfc8af658ef289144a1e01
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-04-28 15:56:47 +0200

    remove comment

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -13,8 +13,6 @@ from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from collasso import simulate, IndepLassoCV, CoopLassoCV
 
-# Consider decreasing q from 10 to 3 (faster and more realistic).
-
 rho = [0, 0.90]
 prob_com = [0.00, 0.05]
 prob_sep = [0.00, 0.05]
